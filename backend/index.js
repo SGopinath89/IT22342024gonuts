985c7cebcb79907503810529b423d92b69df639f
ruwini01 <12345tharanga12345@gmail.com>
2024-05-22 09:58:22 +0530

    Few API endpoints Added
    addtocart, removefromcart and getcart APIs added to backend. And updated in shopContext.jsx

--- backend/index.js
+++ backend/index.js
@@ -237,10 +237,52 @@ app.get('/popular', async(req, res)=>{
 })
 
 
+//creating middleware to fetch user
+const fetchUser = async (req, res, next)=>{
+    const token = req.header('auth-token');
+    if(!token){
+        res.status(401).send({errors:"Please authenticate using valid token"});
+    }
+    else{
+        try{
+            const data = jwt.verify(token, 'gonutswithdonuts');
+            req.user = data.user;
+            next();
+        }
+        catch(error){
+            res.status(401).send({errors:"Please authenticate using valid token"})
+        }
+    }
+}
+
 //creating end point for adding food in cart data
-app.post('addtocart', async(req, res)=>{
-    console.log(req.body);
+app.post('/addtocart',fetchUser, async (req, res)=>{
+    console.log("added", req.body.itemId)
+    let userData = await Users.findOne({_id:req.user.id});
+    userData.cartData[req.body.itemId] += 1;
+    await Users.findOneAndUpdate({_id:req.user.id}, {cartData:userData.cartData});
+    res.send("Added");
 })
+
+//creating end point to remove food in cart data
+app.post('/removefromcart',fetchUser, async (req, res)=>{
+    console.log("removed", req.body.itemId)
+    let userData = await Users.findOne({_id:req.user.id});
+
+    if(userData.cartData[req.body.itemId]>0){
+        userData.cartData[req.body.itemId] -= 1;
+        await Users.findOneAndUpdate({_id:req.user.id}, {cartData:userData.cartData});
+        res.send("Removed");
+    }  
+})
+
+//creating endpoint for get cart data
+app.post('/getcart', fetchUser, async(req, res)=>{
+    console.log("GetCart");
+    let userData = await Users.findOne({_id:req.user.id});
+    res.json(userData.cartData);
+})
+
 
 // Start server
 app.listen(port, (error) => {
