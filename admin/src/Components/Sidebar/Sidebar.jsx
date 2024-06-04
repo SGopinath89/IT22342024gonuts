bcfd7074869890a99db038528ac72a4cf602a206
ruwini01 <12345tharanga12345@gmail.com>
2024-06-04 13:17:46 +0530

    New button 'Messages' Added

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -3,7 +3,8 @@ import './Sidebar.css';
 import { Link } from 'react-router-dom';
 import add_product_icon from '../../assets/add.png';
 import list_product_icon from '../../assets/list.png';
-import orders_icon from '../../assets/box.png'; // Assuming you have an orders icon
+import orders_icon from '../../assets/box.png'; 
+import message from '../../assets/message.png';
 
 const Sidebar = () => {
     const [activeItem, setActiveItem] = useState(null);
@@ -32,6 +33,13 @@ const Sidebar = () => {
                 <div className={`sidebar-item ${activeItem === 2 ? 'clicked' : ''}`} onClick={() => handleItemClick(2)}>
                     <img className="order-i"src={orders_icon} alt="View Orders" />  
                     <p className="order-p">Orders</p>  
+                </div>            
+            </Link>
+
+            <Link to={'/messages'} style={{textDecoration: 'none'}}>
+                <div className={`sidebar-item ${activeItem === 3 ? 'clicked' : ''}`} onClick={() => handleItemClick(3)}>
+                    <img className="message-i"src={message} alt="Messages" />  
+                    <p className="message-p">Messages</p>  
                 </div>            
             </Link>
         </div>
