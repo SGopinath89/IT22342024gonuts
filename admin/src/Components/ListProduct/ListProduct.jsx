7cf2e9491e194abc0a826ab529f22441a5e64cd7
ruwini01 <12345tharanga12345@gmail.com>
2024-06-04 13:16:33 +0530

    Title removed

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -33,7 +33,6 @@ const ListProduct = () => {
     return(
 
         <div className="list-product">
-            <h1>All Products List</h1>
             <div className="listproduct-format-main">
                 <p>Item</p>
                 <p>Title</p>
